e1a3e7caf9156caebbba16f06e409660f4f5e998
VictorAnaya2023 <148127887+VictorAnaya2023@users.noreply.github.com>
2023-10-25 11:57:15 -0700

    Cambios para el formulario de contacto con el captcha v3

--- contact.php
+++ contact.php
@@ -1,69 +1,70 @@
 <?php
 
-//Retrieve form data. 
-//GET - user submitted data using AJAX
-//POST - in case user does not support javascript, we'll use POST instead
-$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
-$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
-$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+require("PHPMailer\src\PHPMailer.php");
+require("PHPMailer\src\SMTP.php");
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl';
+$mail->Host = "mail.esmebeautycare.com";
+$mail->Port = 465; //465  or 587
+$mail->IsHTML(true);
+$mail->Username = "contacto@esmebeautycare.com";
+$mail->Password = "EsmeBeautyCare";
+$mail->SetFrom("contacto@esmebeautycare.com");
+$mail->Subject = "Contacto Desde : Esme Beauty Care";
 
-//flag to indicate which method it uses. If POST set it to 1
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
 
-if ($_POST) $post=1;
-
-//Simple server side validation for POST data, of course, you should validate the email
 if (!$name) $errors[count($errors)] = 'Please enter your name.';
 if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
 if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
 
-//if the errors array is empty, send the mail
+$recaptcha_secret = '6LfmfMgoAAAAAJciCuPI9wIhr0C5mB8EI1vansgU';
+$recaptcha_response = $_POST['token'];
+
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = [
+    'secret' => $recaptcha_secret,
+    'response' => $recaptcha_response,
+];
+
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data),
+    ],
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+$result_json = json_decode($result, true);
+
+if ($result_json['success']) {
+    // reCAPTCHA verification successful
+    // Proceed with your form processing
+} else {
+    // reCAPTCHA verification failed
+    // Handle accordingly (e.g., show an error message)
+}
+
 if (!$errors) {
 
-	//recipient - replace your email here
-	$to = 'contacto@rosaritocentro.com';	
-	//sender - from the form
-	$from = $name . ' <' . $email . '>';
-	
-	//subject and the html message
-	$subject = 'Message via Esme Beauty Care from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
-		       Message: ' . nl2br($comment) . '<br/>';
-
-	//send the mail
-	$result = sendmail($to, $subject, $message, $from);
-	
-	//if POST was used, display the message straight away
-	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
-	//ajax script can react accordingly
-	//1 means success, 0 means failed
+	$mail->Body = "Nombre: {$name}  <br> Correo Electronico: {$email} <br> Mensaje : {$comment}";
+	$mail->AddAddress("contacto@esmebeautycare.com");
+	if(!$mail->Send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
 	} else {
-		echo $result;	
+		echo "OK";
 	}
 
-//if the errors array has values
 } else {
-	//display the errors message
 	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
 	echo '<a href="index.html">Back</a>';
 	exit;
 }
-
-
-//Simple mail function with HTML header
-function sendmail($to, $subject, $message, $from) {
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= 'From: ' . $from . "\r\n";
-	
-	$result = mail($to,$subject,$message,$headers);
-	
-	if ($result) return 1;
-	else return 0;
-}
-
 ?>
--- js/common.js
+++ js/common.js
@@ -1,106 +1,91 @@
 //////CONTACT FORM VALIDATION
+
+function validateField(field, regex) {
+    var value = field.val().trim();
+
+    if (value === '') {
+        field.addClass('error');
+        return true;
+    } else {
+        field.removeClass('error');
+    }
+
+    // Si hay una expresión regular y el valor no coincide, marca como error
+    if (regex && !regex.test(value)) {
+        field.addClass('error');
+        return true;
+    } else {
+        field.removeClass('error');
+    }
+
+    return false;
+}
+
 $(document).ready(function() {
 	
-	//if submit button is clicked
-	$('#submit').click(function () {		
-		
-		//Get the data from all the fields
+	$('#submit').click(function () {	
+		overlay.style.display = 'block';
+
 		var name = $('input[name=name]');
 		var email = $('input[name=email]');
 		var regx = /^([a-z0-9_\-\.])+\@([a-z0-9_\-\.])+\.([a-z]{2,4})$/i;
 		var comment = $('textarea[name=comment]');
-		var returnError = false;
+
+		var nameError = validateField(name);
+		var emailError = validateField(email, regx);
+		var commentError = validateField(comment);
 		
-		//Simple validation to make sure user entered something
-		//Add your own error checking here with JS, but also do some error checking with PHP.
-		//If error found, add hightlight class to the text field
-		if (name.val()=='') {
-			name.addClass('error');
-			returnError = true;
-		} else name.removeClass('error');
-		
-		if (email.val()=='') {
-			email.addClass('error');
-			returnError = true;
-		} else email.removeClass('error');		
-		
-		if(!regx.test(email.val())){
-          email.addClass('error');
-          returnError = true;
-		} else email.removeClass('error');
-		
-		
-		if (comment.val()=='') {
-			comment.addClass('error');
-			returnError = true;
-		} else comment.removeClass('error');
-		
-		// Highlight all error fields, then quit.
-		if(returnError == true){
-			return false;	
+		if (nameError || emailError || commentError) {
+			return false;
 		}
-		
-		//organize the data
-		
-		var data = 'name=' + name.val() + '&email=' + email.val() + '&comment='  + encodeURIComponent(comment.val());
 
-		//disabled all the text fields
-		$('.text').attr('disabled','true');
-		
-		//show the loading sign
-		$('.loading').show();
-		
-		//start the ajax
-		$.ajax({
-			//this is the php file that processes the data and sends email
-			url: "contact.php",	
-			
-			//GET method is used
-			type: "GET",
-
-			//pass the data			
-			data: data,		
-			
-			//Do not cache the page
-			cache: false,
-			
-			//success
-			success: function (html) {				
-				//if contact.php returned 1/true (send mail success)
-				if (html==1) {
+		grecaptcha.ready(function() {
+			grecaptcha.execute('6LfmfMgoAAAAABGo9lwnXrba-RWtAsy7V2OeWXOn', {action: 'contact'}).then(function(token) {
+				// Include the token in your form submission
+				var data = 'name=' + name.val() + '&email=' + email.val() + '&comment='  + encodeURIComponent(comment.val()) + '&token=' + token;
 				
-					//show the success message
-					$('.done').fadeIn('slow');
-					
-					$(".form").find('input[type=text], textarea').val("");
-					
-				//if contact.php returned 0/false (send mail failed)
-				} else alert('Sorry, unexpected error. Please try again later.');				
-			}		
+				$('.text').attr('disabled','true');
+				$('.loading').show();
+				
+				$.ajax({
+					url: "contact.php",    
+					type: "POST",
+					data: data,        
+					cache: false,
+					success: function (msg) {
+						$('#overlay').text('Gracias, recibimos tu mensaje');
+						setTimeout(function () {
+							clearForm();
+							overlay.style.display = 'none';
+						}, 1000);
+					},
+					error: function (error) {
+						$('#overlay').text('Lo sentimos, ocurrió un error :(');
+						setTimeout(function () {
+							overlay.style.display = 'none';
+						}, 1000);
+					}
+				});
+			});
 		});
 		
-		//cancel the submit button default behaviours
 		return false;
 	});	
 });	
 
+function clearForm() {
+    $('input[name=name]').val('');
+    $('input[name=email]').val('');
+    $('textarea[name=comment]').val('');
+}
 
+setTimeout(function(){
+	$('body').addClass('loaded')
+}, 1000);
 
-
-
-	// Page Loader
-	setTimeout(function(){
-		$('body').addClass('loaded')
-	}, 1000);
-	
-	
-	
-	
-	
-	// Menu
-	$(window).on("resize", function () {
-  var positionTop = window.innerHeight / 2;
-  $('nav ul').css('top', positionTop);
-  var marginTop = $('nav ul').height() / 2;
-  $('nav ul').css('margin-top', -marginTop);
+$(window).on("resize", function () {
+	var positionTop = window.innerHeight / 2;
+	$('nav ul').css('top', positionTop);
+	var marginTop = $('nav ul').height() / 2;
+	$('nav ul').css('margin-top', -marginTop);
 }).resize();
